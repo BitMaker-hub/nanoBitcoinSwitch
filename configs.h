346c6f73e73aa960032d7dc347575085d5823cd0
talentpierre <44316281+talentpierre@users.noreply.github.com>
2022-09-20 22:28:54 +0200

    Update configs.h
    removed space

--- configs.h
+++ configs.h
@@ -9,7 +9,7 @@
 #define LNURLP       "LNURLyourpayLink"
                                                               //Copy and paste your LNURLpaylink                    
 #define invoiceKey   "yourKey"                                //Invoice/read key:
-#define switchPin    18                                        //ESP32 pin to set high when payment is done
+#define switchPin    18                                       //ESP32 pin to set high when payment is done
 #define timePin      1000                                     //Time in (ms) to hold this pin on
 #define amount       100                                      //Sats amount expected to receive
 
